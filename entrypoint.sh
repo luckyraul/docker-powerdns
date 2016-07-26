6a29a1bffb80caf4123330bdc8c1d77140592f52
Nikita Tarasov <nikita@mygento.ru>
2016-07-26 17:41:26 +0300

    supermasters

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,19 @@ if [[ "x"$MODE_SLAVE != "x" && "x"$MODE_BIND != "x" ]]; then
 };
 include \"named-superslave.conf\";
     " >> /etc/powerdns/bindbackend.conf
+    echo "$SUPERMASTER_1 ns1" > /etc/powerdns/supermasters.conf
+
+    if [[ "x"$SUPERMASTER_2 != "x" ]]; then
+        echo "$SUPERMASTER_2 ns2" >> /etc/powerdns/supermasters.conf
+    fi
+
+    if [[ "x"$SUPERMASTER_3 != "x" ]]; then
+        echo "$SUPERMASTER_3 ns3" >> /etc/powerdns/supermasters.conf
+    fi
+
+    if [[ "x"$SUPERMASTER_4 != "x" ]]; then
+        echo "$SUPERMASTER_4 ns4" >> /etc/powerdns/supermasters.conf
+    fi
 fi
 
 # Run
